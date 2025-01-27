aa9b511359b6cde5a8491001e3cb9840b9b2ac5f
maaikevanputten <maaikevanputten@maaike.nl>
2025-01-27 17:24:00 +0100

    array method

---
+++ src/main/java/org/example/SolutionMethods.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class SolutionMethods {
+    public static void main(String[] args) {
+        int[] result = createArray(10);
+        System.out.println(Arrays.toString(result));
+    }
+
+    //### Return an Array
+    //Write a static method createArray(int n) that returns an array of size n, filled with numbers from 1 to n. For example, createArray(5) returns [1, 2, 3, 4, 5].
+
+    public static int[] createArray(int n) {
+        int[] nrs = new int[n];
+        for(int i = 1; i <= n; i++) {
+            nrs[i-1] = i;
+        }
+        return nrs;
+    }
+}
